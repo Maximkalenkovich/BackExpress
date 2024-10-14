d52aca62b1c40048857d1029a69cf114cb01f51e
MaximKalenkovich1998 <peterson.maxim@mail.ru>
2024-10-14 15:44:36 +0300

    add prettier

--- src/server.ts
+++ src/server.ts
@@ -3,25 +3,32 @@ import express from 'express';
 const app = express();
 const port = 3001;
 
-app.get('/courses', (req, res) => {
-  res.json([
+const db = {
+  courses: [
     { id: 1, name: 'serverExpress', version: '1.0.0' },
     { id: 2, name: 'serverExpress', version: '1.0.0' },
     { id: 3, name: 'serverExpress', version: '1.0.0' },
     { id: 4, name: 'serverExpress', version: '1.0.0' },
-  ]);
+  ],
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/courses', (req, res) => {
+  if (!db.courses) {
+    res.sendStatus(404).json({ message: 'Course not found' });
+    return;
+  }
+  res.json(db.courses);
 });
 
 app.get('/courses/:id', (req, res) => {
-  const foundCourses = [
-    { id: 1, name: 'serverExpress', version: '1.0.0' },
-    { id: 2, name: 'serverExpress', version: '1.0.0' },
-    { id: 3, name: 'serverExpress', version: '1.0.0' },
-    { id: 4, name: 'serverExpress', version: '1.0.0' },
-  ].find((i) => i.id === parseInt(req.params.id));
+  const foundCourses = db.courses.find((i) => i.id === parseInt(req.params.id));
 
   if (!foundCourses) {
-    res.status(402).json({ message: 'Course not found' });
+    res.sendStatus(404).json({ message: 'Course not found' });
     return;
   }
 
